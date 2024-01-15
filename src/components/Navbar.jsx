112262fae3244fddbcc0e472b2b9fa44f3acf214
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2024-01-15 23:23:01 +0600

    Link done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import reel from '../images/reel.png'
 
 const Navbar = () => {
     return (
-        <div className='border-2 flex items-center gap-10 pl-12 py-4'>
+        <div className='flex items-center gap-10 py-4 pl-12 border-2'>
             <img src={reel} alt="" className='w-[50px] h-[50px]' />
-            <h3  className='font-bold text-xl text-blue-400'>Movies</h3>
-            <h3 className='font-bold text-xl text-blue-400'>Favorites</h3>
+            <Link to="/"  className='text-xl font-bold text-blue-400'>Movies</Link>
+            <Link to="/favorites" className='text-xl font-bold text-blue-400'>Favorites</Link>
         </div>
     );
 };
